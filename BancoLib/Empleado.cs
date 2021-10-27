cc5db3761661f9fd89761c52b7c721e41d45e7a5
RamalloCamila <112988@tecnicatura.frc.utn.edu.ar>
2021-10-26 23:27:20 -0300

    agregando clase empleado y persona

---
+++ BancoLib/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoLib
+{
+    class Empleado : Persona
+    {
+        public Empleado(string nombre, string apellido, long dni, string password)
+                    : base(nombre, apellido, dni, password) { }
+
+    }
+}
---
+++ BancoLib/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoLib
+{
+    class Persona
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public long dni { get; set; }
+        public string password { get; set; }
+
+        public Persona(string nombre, string apellido, long dni, string password)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+            this.password = password;
+        }
+
+        override public string ToString() 
+        {
+            return apellido + ' ' + nombre;
+        }
+
+    }
+}
